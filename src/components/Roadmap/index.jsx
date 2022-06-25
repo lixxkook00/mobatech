4f975d708398f6656b1176a46ecbd0bbc81fe600
Linh <lixxkook@Macbut-M1-cua-Linh.local>
2022-06-25 15:19:37 +0700

    update

--- src/components/Roadmap/index.jsx
+++ src/components/Roadmap/index.jsx
@@ -14,114 +14,114 @@ export default function Roadmap() {
                         </div>
                     </div>
                 </div>
-                <div class="container-x">
-                    <div class="timeline-block timeline-block-right">
-                        <div class="marker"></div>
-                        <div class="timeline-content">
+                <div className="container-x">
+                    <div className="timeline-block timeline-block-right">
+                        <div className="marker"></div>
+                        <div className="timeline-content">
                             <h3>02/2021</h3>
                             <span>Idea and Planning</span>
                         </div>
                     </div>
 
-                    <div class="timeline-block timeline-block-left">
-                        <div class="marker"></div>
-                        <div class="timeline-content">
+                    <div className="timeline-block timeline-block-left">
+                        <div className="marker"></div>
+                        <div className="timeline-content">
                             <h3>05/2021</h3>
                             <span>Market demand survey</span>
                         </div>
                     </div>
 
-                    <div class="timeline-block timeline-block-right">
-                        <div class="marker"></div>
-                        <div class="timeline-content">
+                    <div className="timeline-block timeline-block-right">
+                        <div className="marker"></div>
+                        <div className="timeline-content">
                             <h3>07/2021</h3>
                             <span>Solution thinking</span>
                         </div>
                     </div>
 
-                    <div class="timeline-block timeline-block-left">
-                        <div class="marker"></div>
-                        <div class="timeline-content">
+                    <div className="timeline-block timeline-block-left">
+                        <div className="marker"></div>
+                        <div className="timeline-content">
                             <h3>09/2021</h3>
                             <span>Logical flow and operating plan</span>
                         </div>
                     </div>
 
-                    <div class="timeline-block timeline-block-right">
-                        <div class="marker"></div>
-                        <div class="timeline-content">
+                    <div className="timeline-block timeline-block-right">
+                        <div className="marker"></div>
+                        <div className="timeline-content">
                             <h3>04/2022</h3>
                             <span>Test applying blockchain</span>
                         </div>
                     </div>
 
-                    <div class="timeline-block timeline-block-left">
-                        <div class="marker"></div>
-                        <div class="timeline-content">
+                    <div className="timeline-block timeline-block-left">
+                        <div className="marker"></div>
+                        <div className="timeline-content">
                             <h3>05/2022</h3>
                             <span>Layout Design</span>
                         </div>
                     </div>
 
-                    <div class="timeline-block timeline-block-right">
-                        <div class="marker"></div>
-                        <div class="timeline-content">
+                    <div className="timeline-block timeline-block-right">
+                        <div className="marker"></div>
+                        <div className="timeline-content">
                             <h3>06/2022</h3>
                             <span>Token and basic platform building</span>
                         </div>
                     </div>
 
-                    <div class="timeline-block timeline-block-left">
-                        <div class="marker"></div>
-                        <div class="timeline-content">
+                    <div className="timeline-block timeline-block-left">
+                        <div className="marker"></div>
+                        <div className="timeline-content">
                             <h3>07/2022</h3>
                             <span>Kicks off ICO program</span>
                         </div>
                     </div>
 
-                    <div class="timeline-block timeline-block-right">
-                        <div class="marker"></div>
-                        <div class="timeline-content">
+                    <div className="timeline-block timeline-block-right">
+                        <div className="marker"></div>
+                        <div className="timeline-content">
                             <h3>10/2022</h3>
                             <span>Start stalking program</span>
                         </div>
                     </div>
 
-                    <div class="timeline-block timeline-block-left">
-                        <div class="marker"></div>
-                        <div class="timeline-content">
+                    <div className="timeline-block timeline-block-left">
+                        <div className="marker"></div>
+                        <div className="timeline-content">
                             <h3>12/2022</h3>
                             <span>Application via Android and IOS</span>
                         </div>
                     </div>
 
-                    <div class="timeline-block timeline-block-right">
-                        <div class="marker"></div>
-                        <div class="timeline-content">
+                    <div className="timeline-block timeline-block-right">
+                        <div className="marker"></div>
+                        <div className="timeline-content">
                             <h3>Q4/2023</h3>
                             <span>Beta version of gaming marketplace platform</span>
                         </div>
                     </div>
 
-                    <div class="timeline-block timeline-block-left">
-                        <div class="marker"></div>
-                        <div class="timeline-content">
+                    <div className="timeline-block timeline-block-left">
+                        <div className="marker"></div>
+                        <div className="timeline-content">
                             <h3>Q1/2024</h3>
                             <span>Official Launching</span>
                         </div>
                     </div>
 
-                    <div class="timeline-block timeline-block-right">
-                        <div class="marker"></div>
-                        <div class="timeline-content">
+                    <div className="timeline-block timeline-block-right">
+                        <div className="marker"></div>
+                        <div className="timeline-content">
                             <h3>Q2/2024</h3>
                             <span>Massive community marketing, partner boning and run business</span>
                         </div>
                     </div>
 
-                    <div class="timeline-block timeline-block-left">
-                        <div class="marker"></div>
-                        <div class="timeline-content">
+                    <div className="timeline-block timeline-block-left">
+                        <div className="marker"></div>
+                        <div className="timeline-content">
                             <h3>2025</h3>
                             <span>Research on 3D new platform</span>
                         </div>
